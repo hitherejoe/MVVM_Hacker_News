0f41f9b27c029177f1e791cf057e20ddf474d895
Joe Birch <hitherejoe@Joes-MacBook-Pro-2.local>
2015-03-23 23:13:51 +0000

    Replaced comments list with recycler view and easy adapter

--- app/src/main/java/com/hitherejoe/hackernews/ui/activity/CommentsActivity.java
+++ app/src/main/java/com/hitherejoe/hackernews/ui/activity/CommentsActivity.java
@@ -1,41 +1,134 @@
 package com.hitherejoe.hackernews.ui.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.hitherejoe.hackernews.HackerNewsApplication;
 import com.hitherejoe.hackernews.R;
-import com.hitherejoe.hackernews.ui.fragment.CommentsFragment;
+import com.hitherejoe.hackernews.data.DataManager;
+import com.hitherejoe.hackernews.data.model.Comment;
+import com.hitherejoe.hackernews.data.model.Story;
+import com.hitherejoe.hackernews.ui.adapter.CommentHolder;
+import com.hitherejoe.hackernews.util.ViewUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.app.AppObservable;
+import uk.co.ribot.easyadapter.EasyRecyclerAdapter;
 
 public class CommentsActivity extends BaseActivity {
 
-    public static final String EXTRA_COMMENTS =
-            "com.hitherejoe.HackerNews.ui.activity.CommentsActivity.EXTRA_COMMENTS";
-    public static final String EXTRA_TITLE =
-            "com.hitherejoe.HackerNews.ui.activity.CommentsActivity.EXTRA_TITLE";
+    @InjectView(R.id.progress_indicator)
+    LinearLayout mProgressBar;
+
+    @InjectView(R.id.layout_no_connection)
+    LinearLayout mOfflineLayout;
+
+    @InjectView(R.id.recycler_comments)
+    RecyclerView mCommentsRecycler;
+
+    private static final String TAG = "CommentsActivity";
+    public static final String EXTRA_POST =
+            "com.hitherejoe.HackerNews.ui.activity.CommentsActivity.EXTRA_POST";
+
+    private Story mPost;
+    private DataManager mDataManager;
+    private List<Subscription> mSubscriptions;
+    private EasyRecyclerAdapter<Comment> mEasyRecycleAdapter;
+    private ArrayList<Comment> mComments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_comments);
+        ButterKnife.inject(this);
+        mSubscriptions = new ArrayList<>();
+        mComments = new ArrayList<>();
+        mPost = getIntent().getParcelableExtra(EXTRA_POST);
+        mDataManager = HackerNewsApplication.get().getDataManager();
         setupActionbar();
-        setFragment();
+        setupRecyclerView();
+        checkCanLoadComments();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        for (Subscription subscription : mSubscriptions) subscription.unsubscribe();
     }
 
     private void setupActionbar() {
-        String title = getIntent().getStringExtra(EXTRA_TITLE);
+        String title = mPost.title;
         if (title != null) getSupportActionBar().setTitle(title);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void setFragment() {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment fragment = new CommentsFragment();
-        Bundle b = new Bundle();
-        b.putParcelable(CommentsFragment.ARG_COMMENTS, getIntent().getParcelableExtra(EXTRA_COMMENTS));
-        fragment.setArguments(b);
-        ft.replace(R.id.content_frame, fragment);
-        ft.commit();
+    private void setupRecyclerView() {
+        mCommentsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mEasyRecycleAdapter = new EasyRecyclerAdapter<>(this, CommentHolder.class, mComments);
+        mCommentsRecycler.setAdapter(mEasyRecycleAdapter);
+    }
+
+    @OnClick(R.id.button_try_again)
+    public void onTryAgainClick() {
+        checkCanLoadComments();
+    }
+
+    private void checkCanLoadComments() {
+        if (ViewUtils.isNetworkAvailable(this)) {
+            showHideOfflineLayout(false);
+            retrieveRecursiveCommentsNew(mPost.kids);
+        } else {
+            showHideOfflineLayout(true);
+        }
+    }
+
+    private void showHideOfflineLayout(boolean isOffline) {
+        mOfflineLayout.setVisibility(isOffline ? View.VISIBLE : View.GONE);
+        mCommentsRecycler.setVisibility(isOffline ? View.GONE : View.VISIBLE);
+        mProgressBar.setVisibility(isOffline ? View.GONE : View.VISIBLE);
+    }
+
+    private void retrieveRecursiveCommentsNew(final List<Long> kids) {
+        if (kids != null) {
+            mSubscriptions.add(AppObservable.bindActivity(this,
+                    mDataManager.getStoryComments(kids, 0))
+                    .subscribeOn(mDataManager.getScheduler())
+                    .subscribe(new Subscriber<Comment>() {
+                        @Override
+                        public void onCompleted() {
+                            mProgressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            mProgressBar.setVisibility(View.GONE);
+                            Log.e(TAG, "There was an error retrieving the comments " + e);
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onNext(Comment comment) {
+                            addCommentViews(comment);
+                        }
+                    }));
+        }
+    }
+
+    private void addCommentViews(Comment comment) {
+        mComments.add(comment);
+        mComments.addAll(comment.comments);
+        mEasyRecycleAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/hitherejoe/hackernews/ui/adapter/CommentHolder.java
@@ -0,0 +1,60 @@
+package com.hitherejoe.hackernews.ui.adapter;
+
+import android.text.Html;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hitherejoe.hackernews.R;
+import com.hitherejoe.hackernews.data.model.Comment;
+import com.hitherejoe.hackernews.util.ViewUtils;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.util.Date;
+
+import uk.co.ribot.easyadapter.ItemViewHolder;
+import uk.co.ribot.easyadapter.PositionInfo;
+import uk.co.ribot.easyadapter.annotations.LayoutId;
+import uk.co.ribot.easyadapter.annotations.ViewId;
+
+@LayoutId(R.layout.item_comments_list)
+public class CommentHolder extends ItemViewHolder<Comment> {
+
+    @ViewId(R.id.text_comment)
+    TextView mCommentText;
+
+    @ViewId(R.id.text_post_author)
+    TextView mCommentAuthor;
+
+    @ViewId(R.id.text_post_date)
+    TextView mCommentPoints;
+
+    @ViewId(R.id.container_item)
+    LinearLayout mComment;
+
+    public CommentHolder(View view) {
+        super(view);
+    }
+
+    @Override
+    public void onSetValues(Comment comment, PositionInfo positionInfo) {
+        if (comment.text != null) mCommentText.setText(Html.fromHtml(comment.text.trim()));
+        if (comment.by != null) mCommentAuthor.setText(comment.by);
+        long millisecond = comment.time * 1000;
+        PrettyTime prettyTime = new PrettyTime();
+        mCommentPoints.setText(prettyTime.format(new Date(millisecond)));
+        setCommentIndent(comment.depth);
+    }
+
+    private void setCommentIndent(int depth) {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        float margin = ViewUtils.convertPixelsToDp(depth * 20, getContext());
+        layoutParams.setMargins((int) margin, 0, 0, 0);
+        mComment.setLayoutParams(layoutParams);
+    }
+
+
+}
