d2059a549145678b34741e381a140a8a51f1cdc3
Joe Birch <joe@ribot.co.uk>
2015-08-25 13:05:38 +0100

    Fix broken test module grade file...

--- app/src/test/java/com/hitherejoe/mvvm_hackernews/util/DefaultConfig.java
+++ app/src/test/java/com/hitherejoe/mvvm_hackernews/util/DefaultConfig.java
@@ -1,7 +1,7 @@
 package com.hitherejoe.mvvm_hackernews.util;
 
 public class DefaultConfig {
-    //The api level that Roboelectric will use to run the unit tests
+    //The api level that Robolectric will use to run the unit tests
     public static final int EMULATE_SDK = 21;
     public static final String MANIFEST = "./src/main/AndroidManifest.xml";
 }
